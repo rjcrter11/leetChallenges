0954cca2e5a9b1e785c38a0895c553138a3ee582
Rachel <rjcrter11@gmail.com>
2020-11-03 14:47:18 -0600

    different way to do circular queue

--- queues_and_stacks/circular_queue.py
+++ queues_and_stacks/circular_queue.py
@@ -6,6 +6,7 @@ class CircularQueue:
         self.head = 0
 
     def enQueue(self, value):
+        # use modulu b/c the remainder will give the correct next index
         if self.isFull():
             return False
         self.data[(self.head + self.count) % self.capacity] = value
@@ -38,3 +39,41 @@ class CircularQueue:
         if self.count == self.capacity:
             return True
         return False
+
+
+class circQueue2:
+    def __init__(self, k):
+        self.queue = [None] * k
+        self.head = 0
+        self.tail = -1
+        self.capacity = k
+        self.q_size = 0
+
+    def enqueue(self, value):
+        if self.is_full:
+            return False
+        self.tail = (self.tail + 1) % self.capacity
+        self.queue[self.tail] = value
+        self.q_size += 1
+        return True
+
+    def dequeue(self):
+
+        if self.is_empty():
+            return False
+        self.queue[self.head] = None
+        self.head = (self.head + 1) % self.capacity
+        self.q_size -= 1
+        return True
+
+    def front(self):
+        return -1 if self.is_empty() else self.queue[self.head]
+
+    def rear(self):
+        return -1 if self.is_empty() else self.queue[self.tail]
+
+    def is_full(self):
+        return self.q_size == self.capacity
+
+    def is_empty(self):
+        return self.q_size == 0
