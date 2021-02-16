993d8aa8a5c61aef901dc0bd5fe31876b1b14d52
Rachel <rjcrter11@gmail.com>
2021-02-16 14:29:48 -0600

    number of ways to make change

---
+++ arrays/number_ways_make_change.py
@@ -0,0 +1,25 @@
+'''
+Given an array of distinct positive integers representing coin denominations and 
+a single non-negative integer n represents a target amount of money, write a function that 
+returns the number of ways to make change for that target using the given coin denominations
+
+'''
+
+
+def numberOfWaysToMakeChange(n, denoms):
+    # Write your code here.
+    ways = [0 for amount in range(n+1)]
+    ways[0] = 1
+
+    for denom in denoms:
+        for amount in range(1, n+1):
+            if denom <= amount:
+                ways[amount] += ways[amount-denom]
+                print(ways)
+    return ways[n]
+
+
+num = 6
+denoms = [1, 5]
+
+print(numberOfWaysToMakeChange(num, denoms))
