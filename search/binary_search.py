4e183f9e3bcbe189b2def15ddafc4fc5f9d948b6
Rachel <rjcrter11@gmail.com>
2021-03-11 14:01:43 -0600

    binary search alt

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,7 +1,7 @@
 '''
 Write a function that takes in a sorted array of integers as well as
-a target integer. The function should use the Binary Search algorithm to 
-determine if the target integer is contained in the array and should return 
+a target integer. The function should use the Binary Search algorithm to
+determine if the target integer is contained in the array and should return
 its index if it is, otherwise -1
 '''
 
@@ -27,3 +27,22 @@ array = [1, 5, 23, 111]
 target = 5
 
 print(binarySearch(array, target))
+
+
+def binarySearchAlt(array, target):
+    # Write your code here.
+    left = 0
+
+    right = len(array) - 1
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if array[mid] == target:
+            return mid
+        if target > array[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
