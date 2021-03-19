cc524bbfa2383a43213da826ef7dbb362cf19cf2
Rachel <rjcrter11@gmail.com>
2021-03-19 10:17:23 -0500

    kadanes algo alt

--- arrays/kadanes_algorithm.py
+++ arrays/kadanes_algorithm.py
@@ -21,3 +21,19 @@ def kadanealgo(arr):
 
 array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
 print(kadanealgo(array))
+
+
+def kadanesAlgorithm(array):
+    # Write your code here.
+    maxEndingHere = array[0]
+    maxSoFar = array[0]
+
+    for i in range(1, len(array)):
+        num = array[i]
+        maxEndingHere = max(num, maxEndingHere + num)
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    return maxSoFar
+
+
+array2 = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+print(kadanesAlgorithm(array2))
