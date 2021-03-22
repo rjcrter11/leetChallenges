87d905e40badca449dd0413efe720da14be72ce8
Rachel <rjcrter11@gmail.com>
2021-03-22 08:15:41 -0500

    make array consecutive alt2

--- arrays/make_array_consecutive.py
+++ arrays/make_array_consecutive.py
@@ -1,9 +1,9 @@
 '''
-Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, 
-each statue having an non-negative integer size. Since he likes to make things 
-perfect, he wants to arrange them from smallest to largest so that each statue 
-will be bigger than the previous one exactly by 1. He may need some additional 
-statues to be able to accomplish that. Help him figure out the minimum number of 
+Ratiorg got statues of different sizes as a present from CodeMaster for his birthday,
+each statue having an non-negative integer size. Since he likes to make things
+perfect, he wants to arrange them from smallest to largest so that each statue
+will be bigger than the previous one exactly by 1. He may need some additional
+statues to be able to accomplish that. Help him figure out the minimum number of
 additional statues needed.
 
 Example
@@ -29,3 +29,18 @@ def makeArrayConsecutive(statues):
 
 statues = [6, 2, 3, 8]
 print(makeArrayConsecutive(statues))
+
+
+def consecutiveAlt(statues):
+    return max(statues)-min(statues)-len(statues)+1
+
+
+print(consecutiveAlt(statues))
+
+
+def secondAlt(statues):
+    full = len(range(min(statues), max(statues)+1))
+    return full - len(statues)
+
+
+print(secondAlt(statues))
