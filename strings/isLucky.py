ccc63c2588e68415878bb062831c90e2ffd74ed9
Rachel <rjcrter11@gmail.com>
2021-03-23 08:53:41 -0500

    is lucky

---
+++ strings/isLucky.py
@@ -0,0 +1,36 @@
+'''
+Ticket numbers usually consist of an even number of digits. A ticket number is considered 
+lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+Given a ticket number n, determine if it's lucky or not.
+
+Example
+
+For n = 1230, the output should be
+isLucky(n) = true;
+For n = 239017, the output should be
+isLucky(n) = false.
+'''
+
+
+def isLucky(n):
+    s = str(n)
+
+    first_sum = 0
+    second_sum = 0
+
+    pivot = len(s) // 2
+
+    first_half = s[:pivot]
+    second_half = s[pivot:len(s)]
+
+    for i in first_half:
+        first_sum += int(i)
+    for i in second_half:
+        second_sum += int(i)
+
+    return first_sum == second_sum
+
+
+n = 1230
+print(isLucky(n))
