fff3ada00d7c10c7c09cc4bd237d4daeb17f04af
Rachel <rjcrter11@gmail.com>
2021-05-06 13:17:29 -0500

    word spin

---
+++ strings/word_spin.py
@@ -0,0 +1,31 @@
+'''
+Write a function that takes in a string of one or more words, and returns 
+the same string, but with all five or more letter words reversed 
+(like the name of this kata).
+
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+Examples:
+
+spinWords("Hey fellow warriors") => "Hey wollef sroirraw" 
+spinWords("This is a test") => "This is a test" 
+spinWords("This is another test") => "This is rehtona test"
+'''
+
+
+def spin_words(sentence):
+    words = sentence.split(' ')
+    res = []
+
+    for word in words:
+        if len(word) >= 5:
+            flip = ''.join(reversed(list(word)))
+            res.append(flip)
+        else:
+            res.append(word)
+    return ' '.join(res)
+
+
+sentence = "Hey fellow warriors"
+
+print(spin_words(sentence))
